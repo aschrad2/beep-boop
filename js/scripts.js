18287f2154bd45afce8eef9d23c7214e42491b15
Student <example@example.com>
2020-02-17 11:11:05 -0800

    Added the prompt and the counter that goes up to the number entered

--- js/scripts.js
+++ js/scripts.js
@@ -2,10 +2,22 @@
 
 
 // Front End Logic
-$(document).ready(function() {
-    $("form#play").submit(function(event) {
-        event.preventDefault();
-        var inputNumber = parseInt($("play").value());
-        var beepBoopOutput = beepBoop(inputNumber);
-    });
-});
+
+var inputtedNumber = prompt("What is your number?: ")
+
+currentNumber = 0
+for (i = 0; i < inputtedNumber + 1; i++) {
+
+    console.log("Hello " + currentNumber)
+    currentNumber += 1
+}
+
+
+// $(document).ready(function() {
+//     $("form#play").submit(function(event) {
+//         event.preventDefault();
+//         var inputNumber = parseInt($("play").value());
+//         var beepBoopOutput = beepBoop(inputNumber);
+//     });
+// });
+
