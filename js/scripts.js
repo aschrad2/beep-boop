ea40eb77c9c70946670501860c107f9754ae778a
Student <example@example.com>
2020-02-17 12:26:56 -0800

    Remove unnecessary commented out code

--- js/scripts.js
+++ js/scripts.js
@@ -2,26 +2,6 @@
 
 
 // Front End Logic
-// var inputtedNumber = parseInt(prompt("What is your number?: "));
-
-function numberThrough(inputtedNumber) {
-    currentNumber = 0;
-    for (i = 0; i < inputtedNumber; i++){
-        console.log("<li>" + currentNumber + "<li>");
-        $("ol").append("<li>" + currentNumber);
-        currentNumber += 1;
-
-    };
-};
-
-// function printToDom(results) {
-//     $("ol").append("<li>Hello</li>");
-//     console.log(results);
-//     $("ol").append(results);
-
-// }
-
-
 $(document).ready(function() {
     $("#number-form").submit(function(event) {
         var inputtedNumber = $("input#inputted-number").val();
@@ -30,24 +10,11 @@ $(document).ready(function() {
     });
 });
 
-
-//     $("#button").click(function() {
-//         $("ol").append("<li>" + currentNumber + "<li>")
-//     });
-// });
-
-// for (i = 0; i < inputtedNumber + 1; i++) {
-//     console.log("Hello " + currentNumber)
-//     $('p').append("Hello " + currentNumber)
-//     currentNumber += 1
-// }
-
-
-// $(document).ready(function() {
-//     $("form#play").submit(function(event) {
-//         event.preventDefault();
-//         var inputNumber = parseInt($("play").value());
-//         var beepBoopOutput = beepBoop(inputNumber);
-//     });
-// });
-
+function numberThrough(inputtedNumber) {
+    currentNumber = 0;
+    for (i = 0; i < inputtedNumber; i++){
+        console.log("<li>" + currentNumber + "<li>");
+        $("ol").append("<li>" + currentNumber);
+        currentNumber += 1;
+    };
+};
