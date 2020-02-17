5616d8ae0fca145c10fb464e67c415844016d8be
Student <example@example.com>
2020-02-17 12:44:55 -0800

    Add functionality to convert 13, 21, 32

--- js/scripts.js
+++ js/scripts.js
@@ -6,13 +6,20 @@ function numberThrough(inputtedNumber) {
             $("ol").append("<li>Beep!");
         } else if (currentNumber.toString().indexOf('2') > -1) {
             $("ol").append("<li>Boop!");
+        } else if (currentNumber.toString().indexOf('3') > -1) {
+            $("ol").append("<li>I'm sorry, Dave. I'm afraid I can't do that.");
+        } else if (currentNumber == 13) {
+            $("ol").append("<li>I'm sorry, Dave. I'm afraid I can't do that.");
+        } else if (currentNumber == 21) {
+            $("ol").append("<li>Boop");
+        } else if (currentNumber == 32) {
+            $("ol").append("<li>I'm sorry, Dave. I'm afraid I can't do that.");
         } else {
             $("ol").append("<li>" + currentNumber);
         }
         currentNumber += 1;
     };
 };
-
 
 // Front End Logic
 $(document).ready(function() {
